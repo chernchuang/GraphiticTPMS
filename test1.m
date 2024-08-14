8d0da6cfee59f2a0fdce2f9770c15e3ea5ae54ae
youbet327 <youbet327@gmail.com>
2024-08-14 02:50:37 -0400

    Update test1.m

--- test1.m
+++ test1.m
@@ -1,2 +1,2 @@
 Graphics3D[{ (* ...primitives and directives... *) },
-   BoxRatios->{1,4,9},Triangle[{{0, 0, 0}, {1, 0, 0}, {0, 1, 1}}] (* ...options... *)]
+   (*BoxRatios->{1,4,9}*),Triangle[{{0, 0, 0}, {1, 0, 0}, {0, 1, 1}}] (* ...options... *)]
